ab5b2d74ff26aa115d950ffff01d47907617c4c0
vivdto@gmail.com <vivdto@gmail.com>
2024-03-05 15:57:41 +0530

    Added response for the server

--- basic_http_server/index.js
+++ basic_http_server/index.js
@@ -1,9 +1,18 @@
 const http = require ('http');
 const port = 8080;
 
+function requestHandler(req, res){
+    console.log(req.url);
+
+    res.end('Gotch!');
+
+    // Response to server!
+
+}
+
 //One Port can usually run one program at a time.
 
-const server = http.createServer();
+const server = http.createServer(requestHandler);
 
 server.listen(port, function (err){
     if(err)
@@ -16,5 +25,7 @@ server.listen(port, function (err){
 });
 
 // Runnig on http://localhost:8080/
+
+// 127.0.0.1:8080
 
 // https://nodejs.org/api/http.html
