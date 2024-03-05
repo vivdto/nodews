d48ab052ea428f36f977145999f3bac81eea724e
vivdto@gmail.com <vivdto@gmail.com>
2024-03-05 16:17:43 +0530

    Serving HTML

--- basic_http_server/index.js
+++ basic_http_server/index.js
@@ -1,31 +1,30 @@
-const http = require ('http');
+const http = require('http');
 const port = 8080;
 
-function requestHandler(req, res){
+const fs = require('fs');
+
+function requestHandler(req, res) {
     console.log(req.url);
 
-    res.end('Gotch!');
+    res.writeHead(200, { 'content-type': 'text/html' });
 
-    // Response to server!
-
+    fs.readFile('./index.html', function(err, data) {
+        if (err) {
+            console.log('error', err);
+            //res.end('Error loading the file'); 
+            //return;
+        }
+        res.end(data); 
+        //  after reading the file using fs.readFile, you're not sending any response back to the client. You need to include res.end(data) or similar inside the fs.readFile callback to send the file contents back as the respons
+    });
 }
-
-//One Port can usually run one program at a time.
 
 const server = http.createServer(requestHandler);
 
-server.listen(port, function (err){
-    if(err)
-    {
+server.listen(port, function(err) {
+    if (err) {
         console.log(err);
         return;
     }
-
     console.log("Server is up and running on port: ", port)
 });
-
-// Runnig on http://localhost:8080/
-
-// 127.0.0.1:8080
-
-// https://nodejs.org/api/http.html
