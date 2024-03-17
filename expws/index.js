62921874b76c8a6378b3de9201976e83966a1e65
vivdto@gmail.com <vivdto@gmail.com>
2024-03-17 20:03:25 +0530

    ContactList Application Backend

--- expws/index.js
+++ expws/index.js
@@ -1,38 +1,71 @@
-// Importing necessary modules
-const express = require('express'); // Express framework for Node.js web applications
-const path = require('path'); // Module for working with file and directory paths
+const express = require('express');
+const path = require('path');
+const port = 8000;
 
-// Setting up the port number for the server
-const port = 6999;
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
 
-// Creating an Express application
 const app = express();
 
-// Setting the view engine to EJS (Embedded JavaScript)
 app.set('view engine', 'ejs');
-
-// Setting the directory for views
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
-// Handling GET request to the root URL '/'
-app.get('/', function(req, res){
-    // Rendering the 'home' view using EJS template engine
-    return res.render('home', { title: "Hey!" });
-});
+
+var contactList = [
+    {
+        name: "Arpan",
+        phone: "1111111111"
+    },
+    {
+        name: "Tony Stark",
+        phone: "1234567890"
+    },
+    {
+        name: "Coding Ninjas",
+        phone: "12131321321"
+    }
+]
 
 app.get('/practice', function(req, res){
     return res.render('practice', {
-        title: "Let us Play with ejs"
-
+        title: "Let us play with ejs"
     });
 });
 
-// Starting the server on the specified port
+
+app.get('/', function(req, res){
+
+    return res.render('home',{
+        title: "Contact List",
+        contact_list: contactList
+    });
+})
+app.post('/create-contact', function(req, res){
+
+    contactList.push(req.body);
+    return res.redirect('/');
+ 
+});
+
 app.listen(port, function(err){
-    // Checking for errors while starting the server
-    if(err) {
-        console.log('Error in running the server', err);
+    if (err) {
+        console.log("Error in running the server", err);
     }
-    // Logging a message indicating the server is running
-    console.log('Yup! My Express Server is Running on PORT:', port);
+    console.log('Yup!My Server is running on Port', port);
+})
+
+
+app.get('/delete-contact/', function(req, res){
+    console.log(req.query);
+    let phone = req.query.phone
+
+    let contactindex = contactList.findIndex(contact => contact.phone == phone);
+
+    if(contactindex != -1){
+        contactList.splice(contactindex, 1);
+    }
+
+    return res.redirect('back');
 });
